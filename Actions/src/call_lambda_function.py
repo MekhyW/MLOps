7053832adc52394c772f4a8234e3232e6733e2fd
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-09-25 11:17:21 -0300

    Create call_lambda_function.py

---
+++ Actions/src/call_lambda_function.py
@@ -0,0 +1,26 @@
+import io
+import boto3
+import json
+
+function_name = "wc_felipec13"
+
+# Create a Boto3 client for AWS Lambda
+lambda_client = boto3.client("lambda")
+
+msg = {"body": "hello from mars"}
+
+try:
+    print(f"Message:\n{msg}")
+
+    response = lambda_client.invoke(
+        FunctionName=function_name,
+        InvocationType="RequestResponse",
+        Payload=json.dumps(msg),
+    )
+
+    payload = response["Payload"]
+
+    txt = io.BytesIO(payload.read()).read().decode("utf-8")
+    print(f"\nResponse:\n{txt}")
+except Exception as e:
+    print(e)
